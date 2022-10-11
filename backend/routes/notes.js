d77988b9b3ad1aca7f8e3966d3cab673a3b35938
Kaushal <kaushalkishorroy21@gmail.com>
2022-10-11 11:44:32 +0530

    day

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+var fetchuser = require("./middleware/fetchuser");
+const Notes = require("../models/Notes");
+// Route 1: Get All the notes  : GET "/api/notes/fetchallnotes". Login required
 
-router.get("/", (req, res) => {
-  res.json([]);
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  const notes = await Notes.find({ user: req.user.id });
+  res.json();
+  res.json(notes);
+});
+// Route 1: Add a new note  using post : "/api/notes/addnotes". Login required
+router.get("/addnotes", fetchuser, async (req, res) => {
+  res.json();
+  res.json(notes);
 });
 
 module.exports = router;
